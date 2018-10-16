2b294b36b43adde7866d3d285e906a6c7e43542f
Toblipa <pablo.hermunoz@gmail.com>
2018-10-16 18:28:57 -0300

    Ejercicios hechos en clase

--- ejemplo_aux3/app_de_IN3501/views.py
+++ ejemplo_aux3/app_de_IN3501/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'app_de_IN3501/index.html')
+    contexto = {'cargo1':"Auxiliares",'cargo2':"Profesores", 'cargo3':"Ayudantes"}
+    return render(request, 'app_de_IN3501/index.html', contexto)
 
 
 def welcome(request):
